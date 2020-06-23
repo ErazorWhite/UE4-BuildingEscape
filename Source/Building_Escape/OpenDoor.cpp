fafbc4f9a9d3afd6e05e2bdaf7fc634dc5805824
Erazor <alexeygusak@gmail.com>
2020-06-23 22:16:29 +0300

    Added trigger volume for opening door

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -18,7 +18,7 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw = InitialYaw + 90.f;
+	TargetYaw += InitialYaw;
 }
 
 // Called every frame
@@ -26,13 +26,19 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (CurrentYaw < 179.f)
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Door rotation: %f"), GetOwner()->GetActorRotation().Yaw);
-
-		CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2);
-		FRotator DoorRotation = GetOwner()->GetActorRotation();
-		DoorRotation.Yaw = CurrentYaw;
-		GetOwner()->SetActorRotation(DoorRotation);
+		OpenDoor(DeltaTime);
 	}
+}
+
+void UOpenDoor::OpenDoor(float DeltaTime)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Door rotation: %f"), GetOwner()->GetActorRotation().Yaw);
+	// CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 1.f); // Just another way to change Yaw
+	// CurrentYaw = FMath::FInterpConstantTo(CurrentYaw, TargetYaw, DeltaTime, 45); // Hard way to change Yaw
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2); // Flexible way to change Yaw
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
 }
